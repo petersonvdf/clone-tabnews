3203b0d87b6193cd903457ca428c0bc42f88f304
petersonvdf <59851907+petersonvdf@users.noreply.github.com>
2025-01-12 01:25:46 +0000

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home() {
+    return <h1>Dickinson Vasconcelos Teixeira de Freitas será convocado para o concurso dos Correios para o cargo de Analista de TI.</h1>
+}
+
+export default Home;
