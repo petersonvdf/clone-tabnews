c9894be5d84b5d959cf6f605375f524d844ba998
petersonvdf <59851907+petersonvdf@users.noreply.github.com>
2025-01-18 03:49:41 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>O candidato Dickinson Vasconcelos Teixeira de Freitas será convocado para o concurso dos Correios para o cargo de Analista de TI.</h1>
+  return (
+    <h1>
+      O candidato Dickinson Vasconcelos Teixeira de Freitas será convocado para
+      o concurso dos Correios para o cargo de Analista de TI.
+    </h1>
+  );
 }
 
 export default Home;
